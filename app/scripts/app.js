026113fdfea7aafa12e446aaf5623b3bc8a88923
Summer <summermichiko@gmail.com>
2014-08-28 14:14:18 -0400

    initial commit

---
+++ app/scripts/app.js
@@ -0,0 +1,30 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name quizApp
+ * @description
+ * # quizApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('quizApp', [
+    'ngCookies',
+    'ngResource',
+    'ngRoute'
+  ])
+  .config(function ($routeProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl: 'views/main.html',
+        controller: 'MainCtrl'
+      })
+      .when('/about', {
+        templateUrl: 'views/about.html',
+        controller: 'AboutCtrl'
+      })
+      .otherwise({
+        redirectTo: '/'
+      });
+  });
